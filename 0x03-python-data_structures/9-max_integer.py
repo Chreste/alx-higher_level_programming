cd35cbf893ff082c79dbd3df778511b9a21a730b
CHRISTENA EHAB <kokoehab1999@gmail.com>
2023-09-10 08:17:19 -0700

    commit in python data structures: lists, tuples

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if not my_list:
+    if len(my_list) < 1:
         return None
-    max_value = my_list[0]
-    for number in my_list:
-        if number > max_value:
-            max_value = number
-            return max_value
+    list_copy = my_list.copy()
+    list_copy.sort()
+    return list_copy[-1]
