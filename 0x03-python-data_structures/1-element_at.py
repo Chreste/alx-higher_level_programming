e077e4a62bd283f960bbf8b692e359f5302ac495
CHRISTENA EHAB <kokoehab1999@gmail.com>
2023-09-10 07:04:27 -0700

    commit in python data structures: lists, tuples

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-     if idx < 0 or idx > (len(my_list) - 1):
+    if idx < 0:
         return None
+    
+    m = len(my_list)
+    if idx >= m:
+        return None
+
     return my_list[idx]
