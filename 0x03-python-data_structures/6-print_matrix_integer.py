cd934bd8abba3a26aea048cb1f2c0d6928c799c6
CHRISTENA EHAB <kokoehab1999@gmail.com>
2023-09-10 07:52:32 -0700

    commit in python data structures: lists, tuples

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
      for c in range(len(matrix)):
-        for l in range(len(matrix[l])):
+         for l in range(len(matrix[c])):
             if l != 0:
                 print(" ", end='')
             print("{:d}".format(matrix[c][l]), end='')
-        print()
+            print()
