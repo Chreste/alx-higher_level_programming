939edfe69efac267154c6558026a6b8e442539e4
CHRISTENA EHAB <kokoehab1999@gmail.com>
2023-09-10 06:59:45 -0700

    commit in python data structures: lists, tuples

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-     for ch in range(len( my_list)):
-         print("{:d}".format(my_list[ch]))
+    for ls in my_list:
+        print("{:d}".format(ls))
