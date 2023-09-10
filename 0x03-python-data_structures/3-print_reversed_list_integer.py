dc7cdbb239a372cb0f02fa6f30aebd02070f42ca
CHRISTENA EHAB <kokoehab1999@gmail.com>
2023-09-10 07:33:33 -0700

    commit in python data structures: lists, tuples

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    m = len(my_list)
-    if m == 0:
+    if not my_list:
         return
-    for x in range(m - 1, -1, -1):
-        print(str.format("{}", my_list[x]))
+    for x in reversed(my_list):
+        print(str.format("{}", x))
