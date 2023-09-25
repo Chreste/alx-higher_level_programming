266d45672d16e21db144a70c6299fcecb0a12590
CHRISTENA EHAB <kokoehab1999@gmail.com>
2023-09-25 20:22:09 +0000

    finish

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    t = 0
-    for i in range(x):
-        try:
-            print("{}".format(my_list[i]), end="")
+    i = 0
+    try:
+        while i is not x:
+            print(my_list[i], end="")
             t += 1
-        except IndexError:
-            break
+    except IndexError:
+        break
     print("")
-    return (t)
+    return (i)
