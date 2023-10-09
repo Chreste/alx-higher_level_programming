0e7ae65a9a238c20789a939d20c5d915906ef001
CHRISTENA EHAB <kokoehab1999@gmail.com>
2023-10-09 06:51:40 -0700

    finish

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for lookup method. """
+
+
+def lookup(obj):
+    """function that returns the list of available attributes
+    and methods of an object"""
+
+    return dir(obj)
---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Rectangle class Module"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class"""
+    def __init__(self, width, height):
+        """Initilize rectangle method"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """Method that returns area of rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """String representation method."""
+        return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
