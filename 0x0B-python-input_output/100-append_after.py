56b8294591f50370557c8ed6b92a9d6e1fad5f2c
CHRISTENA EHAB <kokoehab1999@gmail.com>
2023-10-10 07:39:46 -0700

    finish

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -11,9 +11,9 @@ def append_after(filename="", search_string="", new_string=""):
     """
     text = ""
     with open(filename) as r:
-        for l in r:
-            text += l
-            if search_string in l:
+        for line in r:
+            text += line
+            if search_string in line:
                 text += new_string
     with open(filename, "w") as w:
         w.write(text)
