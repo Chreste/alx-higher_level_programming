fd090d2e83a28e4f4655f5fe939d2316ef923f8e
CHRISTENA EHAB <kokoehab1999@gmail.com>
2023-11-14 09:38:29 -0800

    init

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
---create a table called first_table in the current database in your MySQL
+-- create a table called first_table in the current database
 CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
