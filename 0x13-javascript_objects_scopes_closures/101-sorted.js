86a05d600dd394efa09348b7e3f74ecdfda65133
CHRISTENA EHAB <kokoehab1999@gmail.com>
2023-12-12 13:55:38 -0800

    b

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,17 @@
 #!/usr/bin/node
-const dict = require('./101-data.js').dict;
-let newDict = {};
-for (let key in dict) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [key];
-  } else {
-    newDict[dict[key]].push(key);
+const dict = require('./101-data').dict;
+
+const totalist = Object.entries(dict);
+const vals = Object.values(dict);
+const valsUniq = [...new Set(vals)];
+const newDict = {};
+for (const x in valsUniq) {
+  const list = [];
+  for (const k in totalist) {
+    if (totalist[k][1] === valsUniq[x]) {
+      list.unshift(totalist[k][0]);
+    }
   }
+  newDict[valsUniq[x]] = list;
 }
 console.log(newDict);
