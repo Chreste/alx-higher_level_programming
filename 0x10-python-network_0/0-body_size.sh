052873a144b31fe3d3930a5b20325b9b5cbcdc68
CHRISTENA EHAB <kokoehab1999@gmail.com>
2024-01-05 04:49:32 -0800

    python network

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Get the byte size of the HTTP response header for a given URL.
-curl -sI "$1" | grep "Content-Length:" | cut -d' ' -f 2
+curl -sI "$1" | awk '/Content-Length/{print $2}'
