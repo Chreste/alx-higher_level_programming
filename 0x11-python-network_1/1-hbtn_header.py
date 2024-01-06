dc0ea0b62d228a9649f31263d295ec393306cb59
CHRISTENA EHAB <kokoehab1999@gmail.com>
2024-01-05 17:54:50 -0800

    python network

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
+""" 
+displays the value of the X-Request-Id variable found in the header of the response """
 
-import sys
+import urllib.request
+from sys import argv
+
+def main(argv):
+    with urllib.request.urlopen(argv[1]) as response:
+        alx = response.info()
+        if 'X-Request-Id' in alx:
+            print(alx['X-Request-Id'])
+        else:
+            print("X-Request-Id header isn't present in the response")
 
 if __name__ == "__main__":
-    import urllib.request
-    with urllib.request.urlopen(sys.argv[1]) as response:
-        alx = response.info()
-        print(alx['X-Request-Id'])
+    if len(argv) < 2:
+        print("you need to provide a url.")
+        sys.ext(1)
+    main(argv)
