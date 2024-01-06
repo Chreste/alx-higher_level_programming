02537dca7c8e48e1bdc685ad27d2f7ceb30ffb26
CHRISTENA EHAB <kokoehab1999@gmail.com>
2024-01-05 18:17:44 -0800

    python network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
+"""
+Write a Python script that fetches https://alx-intranet.hbtn.io/
+status and using package urllib """
 
-if __name__ == "__main__":
-    import urllib.request
+import sys
+import urllib.request
+
+def main():
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         alx = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(alx)))
         print("\t- content: {}".format(alx))
         print("\t- utf8 content: {}".format(alx.decode('utf-8')))
+
+if __name__ == " __main__":
+    main()
