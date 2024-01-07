604eff79ecfaf797cd3a3121ea9446b001cfab92
CHRISTENA EHAB <kokoehab1999@gmail.com>
2024-01-07 03:12:42 -0800

    python network

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,17 +5,14 @@ and displays the body of the response (decoded in utf-8)."""
 import sys
 from urllib import request, error, parse
 
-def send_post_request(url, email):
-    data = urllib.parse.urlencode({"email": email}).encode('utf-8')
-    req = urllib.request.Request(url, data, method="POST")
-
-    try:
-        with urllib.request.urlopen(req) as response:
-            body = response.read().decode('utf-8')
-            print(body)
-    except urllib.error.HTTPError as e:
-        print("Error code:", e.code)
-
+if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     send_post_request(url, email)
+    data = urllib.parse.urlencode({"email": email}).encode('utf-8')
+    req = urllib.request.Request(url, data, method="POST")
+    with urllib.request.urlopen(req) as response:
+        body = response.read().decode('utf-8')
+        print(body)
+    except urllib.error.HTTPError as e:
+        print("Error code:", e.code)
