e27049bb6d7b8cbbee71114f636df2fee375f082
CHRISTENA EHAB <kokoehab1999@gmail.com>
2024-01-07 06:57:05 -0800

    python network

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Python script that sends a request to the URL and
+   displays the value of a variable in the response header
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
+    res = requests.get(url)
+    commits = res.json()
+    try:
+        for x in range(10):
+            print("{}: {}".format(
+                commits[x].get("sha"),
+                commits[x].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
