1dfe2e738dc8b5774f38dc2fa5df26970b0dc00e
CHRISTENA EHAB <kokoehab1999@gmail.com>
2024-01-07 06:22:00 -0800

    python network

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,7 +6,10 @@ from urllib import request, parse
 from sys import argv
 
 if __name__ == "__main__":
-    data = parse.urlencode({"email": argv[2]}).encode('utf-8')
-    req = request.Request(argv[1], data, method="POST")
-    with request.urlopen(req) as respone:
-        print(response.read().decode('utf-8'))
+    try:
+        data = parse.urlencode({"email": argv[2]}).encode('utf-8')
+        req = request.Request(argv[1], data, method="POST")
+        with request.urlopen(req) as respone:
+            print(response.read().decode('utf-8'))
+    except IndexError:
+        print("Usage")
