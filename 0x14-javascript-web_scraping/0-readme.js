ce4bb5ea2c2da0dd0d2da02a816f7a08fc05e35e
CHRISTENA EHAB <kokoehab1999@gmail.com>
2024-01-30 12:38:26 -0800

    finish

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,8 +4,8 @@ const fileName = process.argv[2];
 const fs = require('fs');
 
 fs.readFile(fileName, 'utf-8', (err, data) => {
-	if (err) {
-		console.error(err);
-	}
-	console.log(data);
+  if (err) {
+    console.error(err);
+  }
+  console.log(data);
 });
